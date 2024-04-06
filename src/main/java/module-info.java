63cb476f6984db6d5034f1ac5a5d8b84217a0bbf
Mykyta Tishkin <87891550+mykytatishkin@users.noreply.github.com>
2024-04-06 22:21:47 +0300

    Project Achitecture

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.marketcourseprojectfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.marketcourseprojectfx to javafx.fxml;
+    exports com.example.marketcourseprojectfx;
+    exports com.example.marketcourseprojectfx.Controller;
+    opens com.example.marketcourseprojectfx.Controller to javafx.fxml;
+}
