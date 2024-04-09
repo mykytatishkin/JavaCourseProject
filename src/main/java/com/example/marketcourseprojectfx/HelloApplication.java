63b73659be2af9f5dda41ed60eb0fbbe7d882bef
Mykyta Tishkin <87891550+mykytatishkin@users.noreply.github.com>
2024-04-09 13:37:39 +0300

    PopUp

--- src/main/java/com/example/marketcourseprojectfx/HelloApplication.java
+++ src/main/java/com/example/marketcourseprojectfx/HelloApplication.java
@@ -4,6 +4,9 @@ import com.example.marketcourseprojectfx.Controller.DbController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +18,21 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setScene(scene);
         stage.show();
+
+        // Создаем всплывающее окно
+        Stage popupStage = new Stage();
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        popupStage.setTitle("Popup Window");
+
+        StackPane popupRoot = new StackPane();
+        Label popupLabel = new Label("App in development mode");
+        popupRoot.getChildren().add(popupLabel);
+
+        Scene popupScene = new Scene(popupRoot, 200, 100);
+        popupStage.setScene(popupScene);
+
+        // Показываем всплывающее окно
+        popupStage.show();
     }
 
     public static void main(String[] args) {
